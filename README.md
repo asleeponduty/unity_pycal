4b03c7d0404f72ddb924d9be93f9a01a73dc2ded
asleeponduty <lukedrong@gmail.com>
2021-05-19 04:26:41 -0600

    + Html->Png->Png-square Pipeline

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
-# .ical to HTML converter
-The first step of this task is to consume an existing .ical, convert it into representative timezones, 
-and produce HTML that meets our standards for formatting. 
+# .ical -> HTML -> PNG pipeline
+The goal of this project is to take an ical, parse the next two weeks, convert it into various local timezones, and
+produce a square image formatted to our needs for display in game. 
 
-## What's next
-Next steps in this task list are the following:
-
-1. Complete the calendar header
-2. Automate image generation of the calendar in each locale. (Will likely use firefox in headless mode for this)
-3. Manipulate the images using python-opencv to construct the calendar 1k (or 2k) square image, compliant with the scroll shader. 
-4. Encode each image into a single-frame mp4 and upload to a cloud service of our choosing (gdrive)
+## What this does
+1. Consume an existing .ical, convert it into representative timezones, 
+and generate HTML in a pleasing format
+  * Append this generated html to a spacing header to complete the format
+2. Use Firefox in headless mode to generate 512x2048px images of the calendar in each locale
+3. Manipulate the images using python-opencv to construct the calendar 1k square image, compliant with the scroll shader. 
+4. [TODO] Encode each image into a single-frame mp4 
+5. [TODO] upload to a cloud service of our choosing (gdrive)
 
 ## Steps in Unity
 
 <ol start="5">
 <li>Configure the unity project to discriminate between the user's locales</li>
 <li>Use the users' locale to select the correct asset url</li>
+<li>Rewrite the shader to work with 1k images instead of 4k images</li>
 <li>Feed the correct url to the <a href="https://github.com/AoiKamishiro/VRC_UdonEventCalendar">Calendar Prefab</a></li>
 </ol>
+
+
+## References you need for this project:
+Python3+
+### Python Libraries (pip):
+* ics
+* opencv-python
+* urllib3
+
+### Fonts
+Google's ["Noto Sans JP"]("https://fonts.google.com/specimen/Noto+Sans+JP") font needs to be extracted to the fonts/Noto_Sans_JP directory
--- main.py
+++ main.py
@@ -5,11 +5,18 @@ This generates three html documents, in the timezones declared in "tzs"
 from the ical accessible at "url"
 """
 import argparse
+from datetime import timedelta
 from dateutil import tz
 import html
 from ics import *
 import os
+from string import Template
 import urllib.request
+import subprocess
+
+import cv2
+
+LOOKAHEAD = 14  # Days
 
 
 def filesafe_str(in_str):
@@ -20,55 +27,57 @@ def generate_calendars(ical_url, canonical_tzs, use_cache=False):
     head = "<!DOCTYPE html>\n<html>\n<head>" + \
            '<meta charset="UTF-8">' + \
            '<link rel=\"stylesheet\" href=\"style.css\"/>' + "</head><body>\n"
-    tail = "</body></html>"
+    tail = "</div>\n</body></html>"
+
+    body_pre = Template(
+        '<div class="top">\n'
+        '\t<div class="scrollbar"></div>\n' +
+        '\t<div class="header"><img src="banner/current.png"></div>\n' +
+        '\t<div class="spacer"></div>\n' +
+        '\t<div class="footer">Poonis Technologies - $timezone</div>\n' +
+        '</div>\n'
+        '<div class="bottom">\n' +
+        '\t<div class="scrollbar"></div>\n')
 
     if use_cache and os.path.exists("calendar.ical"):
-        print("Using Cached")
+        # print("Using Cached")
         with open("calendar.ical", "r") as f:
             ics_string = f.read()
     else:
-        print("Requesting Calendar")
+        # print("Requesting Calendar")
         with urllib.request.urlopen(ical_url) as response:
             ics_string = response.read()
         if use_cache:
             with open('calendar.ical', "wb") as f:
                 f.write(ics_string)
-    tz_list = []
-    for can_tz in canonical_tzs:
-        tz_list.append(tz.gettz(can_tz))
 
-    for loc_tz in tz_list:
-        print(f"Using TZ {loc_tz}")
+    files = []
+
+    for can_tz in canonical_tzs:
+        loc_tz = tz.gettz(can_tz)
         window_start = datetime.now(loc_tz)
-        # For generating files with the UTC offset instead, use this:
+        # For generating files with the UTC offset in the filename instead, use this:
         # offset = loc_tz.utcoffset(window_start)
         # offset_h = str(offset.seconds / 3600)
-        window_end = window_start + timedelta(days=7)
+        window_end = window_start + timedelta(days=LOOKAHEAD)
         events = get_events_from_ics(ics_string, window_start, window_end, loc_tz)
-        with open(f"cal_{filesafe_str(str(loc_tz))}.html", "w", encoding="utf-8") as out:
+        fname = f"cal_{filesafe_str(str(loc_tz))}.html"
+        files.append((fname, can_tz))
+        with open(fname, "w", encoding="utf-8") as out:
             out.write(head)
+            out.write(body_pre.substitute(timezone=can_tz))
             out.write(f"<div class=\"calendar\"><table>\n")
             day = None
             for e in events:
                 start = e['startdt'].astimezone(tz=loc_tz)
                 e_date = start.strftime('%b %d')
                 if e_date != day:
-                    # if day is not None:
-                    #     out.write("</table>\n")
-                    # out.write(f"\t<tr><th></th><th></th></th>\n")
                     out.write(f"\t<tr><td colspan=\"2\" class=\"date\">{e_date}</td></tr>\n")
-                    print(e_date)
                 day = e_date
-                # mt = False
-                # if localzone == timezone.utc:
-                #     mt = True
                 if e['allday']:
-                    print(f"{html.escape(e['summary'])} - All Day")
-                    #
                     out.write(f"\t<tr><td class=\"starttime\">&nbsp;</td>" +
                               f"<td class=\"allday summary\">{html.escape(e['summary'])}</td></tr>\n")
                 else:
-                    print(f"{start.strftime('%H:%M')} - {html.escape(e['summary'])}")
                     end = e.get('enddt', None)
                     if end:
                         end = end.astimezone(tz=loc_tz)
@@ -78,13 +87,40 @@ def generate_calendars(ical_url, canonical_tzs, use_cache=False):
                     else:
                         out.write(f"\t<tr><td class=\"starttime\">{start.strftime('%H:%M')}</td>" +
                                   f"<td class=\"summary\">{html.escape(e['summary'])}</td></tr>\n")
-                    # if mt:
-                    #     print(f"{local_time.strftime('%H:%M')} - {e['summary']}")
-                    # else:
-                    #     print(f"{local_time.strftime('%I:%M %p')} - {e['summary']}")
             out.write("</table>\n</div>\n")
             out.write(tail)
-        print("* " * 10)
+        print("*", end="")
+    return files
+
+
+def generate_with_firefox(result_tuples):
+    calendar_images_tmp = []
+    for filename, can_tz in result_tuples:
+        cal_fname = f'calendar_{filesafe_str(can_tz)}.png'
+        cal_path = os.path.abspath("output/tmp") + os.sep + cal_fname
+        calendar_images_tmp.append(cal_path)
+        subprocess.run(
+            'c:\\Program Files\\Mozilla Firefox\\firefox.exe' +
+            ' --headless --profile TEMP_FIREFOX --no-remote' +
+            f' --screenshot {cal_path}' +
+            f' file:///{os.path.abspath(filename)} ' +
+            ' --window-size=512,2048')
+        print("*", end="")
+
+    return calendar_images_tmp
+
+
+def reshape_with_ocv(image_paths):
+    for full_path in image_paths:
+        image = cv2.imread(full_path)
+        sz = image.shape  # y, x, z
+        im_l = image[:sz[1]*2, :, :]
+        im_r = image[sz[1]*2:, :, :]
+        im_h = cv2.hconcat([im_l, im_r])
+        new_path = full_path.replace(os.sep + "tmp", "")
+        cv2.imwrite(new_path, im_h)
+        os.remove(full_path)
+        print("*", end="")
 
 
 if __name__ == '__main__':
@@ -108,4 +144,13 @@ if __name__ == '__main__':
         else:
             tzs = [args.tzs]
 
-    generate_calendars(url, tzs, args.cache)
+    total = len(tzs)
+    print(f"Generating Calendars for {total} timezones")
+    cal_results = generate_calendars(url, tzs, args.cache)
+    print(']')
+    print(f"Rendering images from html\n[", end="")
+    pre_imgs = generate_with_firefox(cal_results)
+    print(']')
+    print(f"Formatting to Square with OpenCV\n[", end="")
+    reshape_with_ocv(pre_imgs)
+    print(']')
