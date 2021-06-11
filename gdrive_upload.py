2b0c399b6fb2c58820d2d642963795327e0c2c30
asleeponduty <lukedrong@gmail.com>
2021-06-11 11:49:53 -0600

    Catch and handle refresh-token revocation

--- gdrive_upload.py
+++ gdrive_upload.py
@@ -7,6 +7,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 from googleapiclient import errors
+from google.auth.exceptions import RefreshError
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 
 from gdrive.upload_file_dict import file_dict
@@ -29,7 +30,11 @@ def setup_service():
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                os.remove('gdrive/token.json')
+                raise
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'gdrive/client_secret.json', SCOPES)
